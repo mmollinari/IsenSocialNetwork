0d7c2a35187911f39a9c5375c16b66264282964b
mmollinari <marc.mollinari@gmail.com>
2022-02-22 13:40:44 +0100

    Ajout du binding

--- app/src/main/java/fr/isen/isensocialnetwork/MainActivity.kt
+++ app/src/main/java/fr/isen/isensocialnetwork/MainActivity.kt
@@ -2,10 +2,14 @@ package fr.isen.isensocialnetwork
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import fr.isen.isensocialnetwork.databinding.ActivityMainBinding
 
+lateinit var binding: ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
