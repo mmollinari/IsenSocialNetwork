cc0f10fde0d218c32f099e080317ee997fb8c5a8
mmollinari <marc.mollinari@gmail.com>
2022-02-22 13:42:47 +0100

    Modification du titre

--- app/src/main/java/fr/isen/isensocialnetwork/MainActivity.kt
+++ app/src/main/java/fr/isen/isensocialnetwork/MainActivity.kt
@@ -2,10 +2,13 @@ package fr.isen.isensocialnetwork
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<TextView>(R.id.myTitle).text = "Hello ISEN"
     }
 }
